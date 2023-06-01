959eb8a76cbbc40cb9ea7384d3eb6f700b50f253
Rino <rino@emotiongraphics.jp>
2023-06-02 04:36:05 +0900

    feat: count the number of text and separate into segments

--- index.js
+++ index.js
@@ -6,20 +6,20 @@
 import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import axios from 'axios';
 import xml2js from 'xml2js';
+import TwitterTextParser from 'twitter-text';
+import { htmlToText } from 'html-to-text';
 
 // read .env file and set constants
 import dotenv from 'dotenv';
 dotenv.config();
 const IFTTT_EVENT_NAME = process.env.IFTTT_EVENT_NAME;
+const IFTTT_EVENT_NAME_WITH_IMAGE = process.env.IFTTT_EVENT_NAME_WITH_IMAGE;
 const IFTTT_KEY = process.env.IFTTT_KEY;
 const TABLE_NAME = process.env.TABLE_NAME;
 const RSS_FEED_URL = process.env.RSS_FEED_URL;
 const DYNAMODB_REGION = process.env.DYNAMODB_REGION;
 const MAX_ITEMS = parseInt(process.env.MAX_ITEMS, 10);
 const LOG_LEVEL = process.env.LOG_LEVEL;
-
-const webhookUrl = `https://maker.ifttt.com/trigger/${IFTTT_EVENT_NAME}/json/with/key/${IFTTT_KEY}`;
-if (LOG_LEVEL === 'debug') console.log('webhookUrl:', webhookUrl);
 
 // instantiate the DynamoDB client
 const ddb = new DynamoDBClient({ region: DYNAMODB_REGION });
@@ -58,7 +58,7 @@ export const handler = async (event) => {
         // limit to MAX_ITEMS
         // this code assumes that the items in the RSS Feed array are
         // ordered from newest to oldest.
-        const latestItems = items.slice(0, MAX_ITEMS);
+        const latestItems = items.slice(0, MAX_ITEMS).reverse();
         if (LOG_LEVEL === 'debug') console.log('Latest items:', latestItems);
 
         // calling IFTTT webhook could take time, so it is necessary
@@ -70,34 +70,77 @@ export const handler = async (event) => {
             const pubDate = new Date(item.pubDate[0]).getTime();
             if (LOG_LEVEL === 'debug') console.log('pubDate:', pubDate);
 
+            if (item.category && item.category.find(cat => cat.toLowerCase() === 'nocrosspost')) {
+                return Promise.resolve(); // skip this item
+            }
+
             // process only items newer than the last processed one
             if (!lastProcessedPubDate || lastProcessedPubDate < pubDate) {
                 // extract permalink, pubDate and the content
                 // they should exist in every post
                 const permalink = item.link[0];
                 const published = item.pubDate[0];
-                const content = item.description[0];
-                const postData = { permalink, published, content };
+                const content = htmlToText(item.description[0]);
 
-                // if media is attached...
-                if (item['media:content']) {
-                    // get the url of the first media file
-                    postData.media = item['media:content'][0].$.url;
+                // parse the content into segments
+                let contentSegments = [];
+                let remainingContent = content;
+                let segment = '';
+                let separator = '====';
+
+                while (remainingContent.length > 0) {
+                    let separatorIndex = remainingContent.indexOf(separator);
+                    if (separatorIndex >= 0 && separatorIndex < 280) {
+                        segment = remainingContent.substring(0, separatorIndex);
+                        remainingContent = remainingContent.substring(separatorIndex + separator.length);
+                    } else {
+                        let potentialSegment = remainingContent.substring(0, 280);
+                        let segmentLength = TwitterTextParser.parseTweet(potentialSegment).weightedLength;
+
+                        while (segmentLength > 280) {
+                            potentialSegment = potentialSegment.substring(0, potentialSegment.length - 1);
+                            segmentLength = TwitterTextParser.parseTweet(potentialSegment).weightedLength;
+                        }
+
+                        segment = potentialSegment;
+                        separatorIndex = remainingContent.indexOf(separator);
+
+                        if (separatorIndex === segment.length) {
+                            remainingContent = remainingContent.substring(separatorIndex + separator.length);
+                        } else {
+                            remainingContent = remainingContent.substring(segment.length);
+                        }
+                    }
+                    contentSegments.push(segment);
                 }
-                // otherwise...
-                else {
-                    // set media url as no image placeholder
-                    postData.media = 'no_image_card.png';
+
+                let firstSegment = true;
+                for (const segment of contentSegments) {
+                    let iftttEventName = IFTTT_EVENT_NAME;
+
+                    const postData = { value1: segment };
+
+                    // if media is attached...
+                    if (firstSegment && item['media:content']) {
+                        // get the url of the first media file
+                        postData.value2 = item['media:content'][0].$.url;
+                        iftttEventName = IFTTT_EVENT_NAME_WITH_IMAGE;
+                    }
+                    if (LOG_LEVEL === 'debug') console.log('postData:', postData);
+
+                    // update the last processed item's date
+                    if (!newProcessedPubDate || newProcessedPubDate < pubDate) newProcessedPubDate = pubDate;
+                    if (LOG_LEVEL === 'debug') console.log('Renewed pubDate:', newProcessedPubDate);
+
+                    // make an IFTTT webhook request in sync
+                    const webhookUrl = `https://maker.ifttt.com/trigger/${iftttEventName}/with/key/${IFTTT_KEY}`;
+                    await axios.post(webhookUrl, postData,
+                        {headers: {'Content-Type': 'application/json'}}
+                    );
+
+                    firstSegment = false;
                 }
-                if (LOG_LEVEL === 'debug') console.log('postData:', postData);
-
-                // update the last processed item's date
-                if (!newProcessedPubDate || newProcessedPubDate < pubDate) newProcessedPubDate = pubDate;
-                if (LOG_LEVEL === 'debug') console.log('Renewed pubDate:', newProcessedPubDate);
-
-                // make an IFTTT webhook request in sync
-                const response = await axios.post(webhookUrl, postData);
-                return response;
+                return Promise.resolve;
             }
             else {
                 return Promise.resolve();
